db55195f7116679bf979c017711f70658657a9ef
Ha Do <hadm.haui@gmail.com>
2022-08-18 14:14:56 +0700

    add BrowserAnimationsModule

--- issue-tracker/src/app/app.module.ts
+++ issue-tracker/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ClarityModule
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
