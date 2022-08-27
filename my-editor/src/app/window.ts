19a86e863225588f2f26837fdb2c0444b383bcb0
Ha Do <hadm.haui@gmail.com>
2022-08-27 14:08:16 +0700

    Communicating between Angular and Electron

---
+++ my-editor/src/app/window.ts
@@ -0,0 +1,7 @@
+import { InjectionToken } from '@angular/core';
+export const WINDOW = new InjectionToken<Window>('Global window object', {
+  factory: () => window
+});
+export interface ElectronWindow extends Window {
+  require(module: string): any;
+}
