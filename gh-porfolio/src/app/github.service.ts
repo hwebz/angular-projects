eb5f34f9d1a2a4edfa2e7aac35418fafc4fee86e
Ha Do <hadm.haui@gmail.com>
2022-08-31 13:51:12 +0700

    show github user information on personal info component

--- gh-porfolio/src/app/github.service.ts
+++ gh-porfolio/src/app/github.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +10,8 @@ import { HttpClient } from '@angular/common/http';
 export class GithubService {
 
   constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/${environment.username}`);
+  }
 }
